5192d1d16d92aee98525ba0ff3a680ac2f44f0a5
psychehose <psychehose@gmail.com>
2024-12-02 22:07:03 +0900

    add: expr cell 구현 및 main 함수 구현

--- src/Cell/ExprCell.cpp
+++ src/Cell/ExprCell.cpp
@@ -1,13 +1,15 @@
 #include "ExprCell.h"
 #include "Table.h"
 
-ExprCell::ExprCell(int x, int y, Table *table, std::string data) : Cell(x, y, table), data(data) {}
-
+ExprCell::ExprCell(int x, int y, Table *table, std::string data) : Cell(x, y, table), data(data)
+{
+    parse_expression();
+}
 ExprCell::~ExprCell() {}
 
 std::string ExprCell::stringify()
 {
-    return std::string();
+    return std::to_string(to_numeric());
 }
 
 int ExprCell::to_numeric()
@@ -57,9 +59,67 @@ int ExprCell::to_numeric()
 
 int ExprCell::precedence(char c)
 {
+    // 연산자 우선 순위
+
+    switch (c)
+    {
+    case '(':
+    case '{':
+    case '[':
+        return 0;
+    case '+':
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+        return 2;
+    }
+
     return 0;
 }
 
 void ExprCell::parse_expression()
 {
+    // 중위 표기법 -> 후위 표기법 변환
+    // 3 + 4 * 5 + 4 * (7 - 2) -> 3 4 5 * + 4 7 2 - * +
+    MyExcel::Stack stack;
+
+    // 수식 전체를 () 로 둘러 사서 exp_vec 에 남아있는 연산자들이 push 되게
+    data.insert(0, "(");
+    data.push_back(')');
+
+    for (int i = 0; i < data.length(); ++i)
+    {
+        if (isalpha(data[i])) // cell인 경우
+        {
+            exp_vec.push_back(data.substr(i, 2));
+            ++i;
+        }
+        else if (isdigit(data[i])) // 숫자 한자리 가정
+        {
+            exp_vec.push_back(data.substr(i, 1));
+        }
+        else if (data[i] == '(' || data[i] == '[' || data[i] == '{')
+        {
+            stack.push(data.substr(i, 1));
+        }
+        else if (data[i] == ')' || data[i] == ']' || data[i] == '}')
+        {
+            std::string s = stack.pop();
+
+            while (s != "(" && s != "[" && s != "{")
+            {
+                exp_vec.push_back(s);
+                s = stack.pop();
+            }
+        }
+        else if (data[i] == '+' || data[i] == '-' || data[i] == '*' || data[i] == '/')
+        {
+            while (!stack.is_empty() && precedence(data[i]) <= precedence(stack.peek()[0]))
+            {
+                exp_vec.push_back(stack.pop());
+            }
+            stack.push(data.substr(i, 1));
+        }
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,24 @@
 #include "TxtTable.h"
 #include "Cell/Cell.h"
 
+#include "Cell/NumberCell.h"
+#include "Cell/StringCell.h"
+#include "Cell/DateCell.h"
+#include "Cell/ExprCell.h"
+
 int main(int argc, char *argv[])
 {
     TxtTable table(5, 5);
 
     std::ofstream out("test.txt");
 
-    table.reg_cell(new Cell("Hello World", 0, 0, &table), 0, 0);
-    table.reg_cell(new Cell("C++", 0, 1, &table), 0, 1);
+    table.reg_cell(new NumberCell(1, 1, &table, 2), 1, 1);
+    table.reg_cell(new NumberCell(1, 2, &table, 3), 1, 2);
+    table.reg_cell(new NumberCell(2, 1, &table, 4), 2, 1);
+    table.reg_cell(new NumberCell(2, 2, &table, 5), 2, 2);
 
-    table.reg_cell(new Cell("Programming", 1, 1, &table), 1, 1);
+    table.reg_cell(new ExprCell(3, 2, &table, "B2+B3*(C2+C3-2)"), 3, 2);
+    table.reg_cell(new StringCell(3, 1, &table, "B2 + B3 * ( C2 + C3 - 2 ) = "), 3, 1);
 
     std::cout << std::endl
               << table;
